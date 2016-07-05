0cf609ea77d860362c8169343d7263594ec5307c
mj_caabay <caabaymarcjesus@gmail.com>
2016-07-05 08:20:32 +0800

    push magento

--- app/code/Smartwave/Porto/Model/Config/Settings/Footer/Top/Block.php
+++ app/code/Smartwave/Porto/Model/Config/Settings/Footer/Top/Block.php
@@ -5,11 +5,11 @@ class Block implements \Magento\Framework\Option\ArrayInterface
 {
     public function toOptionArray()
     {
-        return [['value' => '', 'label' => __('Do not show')], ['value' => 'custom', 'label' => __('Custom Block')]];
+        return [['value' => '', 'label' => __('Do not show')], ['value' => 'custom', 'label' => __('Custom Block')], ['value' => 'newsletter', 'label' => __('Newsletter Subscribe')]];
     }
 
     public function toArray()
     {
-        return ['' => __('Do not show'), 'custom' => __('Custom Block')];
+        return ['' => __('Do not show'), 'custom' => __('Custom Block'), 'newsletter' => __('Newsletter Subscribe')];
     }
 }
